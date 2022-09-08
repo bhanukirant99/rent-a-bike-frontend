11e58edb15c1863694f463c190d59c6698af33b4
Bhanu Kiran <bhanukirant99@gmail.com>
2022-09-08 22:38:48 +0530

    adding goodgle auth

--- src/lib/layout/index.tsx
+++ src/lib/layout/index.tsx
@@ -1,8 +1,8 @@
 import { Box } from "@chakra-ui/react";
 import type { ReactNode } from "react";
-
 import Footer from "./Footer";
 import Header from "./Header";
+import LoginPage from "./loginPage";
 
 type LayoutProps = {
   children: ReactNode;
@@ -13,9 +13,10 @@ const Layout = ({ children }: LayoutProps) => {
     <Box margin="0 auto" maxWidth={800} transition="0.5s ease-out">
       <Box margin="8">
         <Header />
-        <Box as="main" marginY={22}>
+        {/* <Box as="main" marginY={22}>
           {children}
-        </Box>
+        </Box> */}
+        <LoginPage />
         <Footer />
       </Box>
     </Box>
